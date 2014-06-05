7cf4c05d583fbdcebba893154b9d0db816812a33
fabiano <fabianorosas@gmail.com>
2014-06-05 15:21:05 -0300

    Fixed critical issue
    Added `equals()` and `hashCode()` methods to ensure consistency with
    `compareTo()`

--- src/main/java/com/brackeen/scared/entity/Entity.java
+++ src/main/java/com/brackeen/scared/entity/Entity.java
@@ -143,7 +143,30 @@ public class Entity implements Comparable<Entity> {
         }
         return false;
     }
-    
+
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Float.floatToIntBits(distanceFromCamera);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Entity other = (Entity) obj;
+		if (Float.floatToIntBits(distanceFromCamera) != Float
+				.floatToIntBits(other.distanceFromCamera))
+			return false;
+		return true;
+	}
+
     // Sort back-to-front
     public int compareTo(Entity t) {
         if (distanceFromCamera < t.distanceFromCamera) {
